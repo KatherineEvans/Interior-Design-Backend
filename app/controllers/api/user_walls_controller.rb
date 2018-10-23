edae4bb4f3a256e5d9312f4e55e07a40301af90c
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-10-22 19:14:10 -0500

    RESTful user_walls controller - create action

--- app/controllers/api/user_walls_controller.rb
+++ app/controllers/api/user_walls_controller.rb
@@ -1,4 +1,16 @@
 class Api::UserWallsController < ApplicationController
   before_action :authenticate_user
 
+  def create
+    user_wall = UserWall.new(
+      art_id: params[:art_id],
+      wall_id: params[:wall_id], 
+      )
+    if user_wall.save
+      render json: {message: 'Saved successfully'}, status: :created
+    else 
+      render json: {errors: user_wall.errors.full_messages}, status: :bad_request
+    end
+  end
+
 end
